16317fcdcb635c767a6f94906d5d96e1f8d8fb91
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 14:16:36 +0530

    creating crud services

--- examserver/src/main/java/com/exam/ExamserverApplication.java
+++ examserver/src/main/java/com/exam/ExamserverApplication.java
@@ -25,26 +25,26 @@ public class ExamserverApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Starting Code");
-
-        User user = new User();
-
-        user.setFirstName("sam");
-        user.setLastName("cody");
-        user.setUsername("samcoder");
-        user.setPassword("abc");
-        user.setEmail("abc@gmail.com");
-        user.setProfile("default.png");
-
-        Role role1 = new Role();
-        role1.setRoleId(44L);
-        role1.setRoleName("ADMIN");
-
-        Set<UserRole> userRoleSet = new HashSet<>();
-        UserRole userRole = new UserRole();
-        userRole.setRole(role1);
-        userRole.setUser(user);
-        userRoleSet.add(userRole);
-        User user1 = this.userService.createUser(user, userRoleSet );
-        System.out.println(user1.getUsername());
+//
+//        User user = new User();
+//
+//        user.setFirstName("sam");
+//        user.setLastName("cody");
+//        user.setUsername("samcoder");
+//        user.setPassword("abc");
+//        user.setEmail("abc@gmail.com");
+//        user.setProfile("default.png");
+//
+//        Role role1 = new Role();
+//        role1.setRoleId(44L);
+//        role1.setRoleName("ADMIN");
+//
+//        Set<UserRole> userRoleSet = new HashSet<>();
+//        UserRole userRole = new UserRole();
+//        userRole.setRole(role1);
+//        userRole.setUser(user);
+//        userRoleSet.add(userRole);
+//        User user1 = this.userService.createUser(user, userRoleSet );
+//        System.out.println(user1.getUsername());
     }
 }
