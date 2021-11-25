caa5df78b817d63f492d9e3de7b1caa354e1ccd9
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 12:36:42 +0530

    Implementing service

---
+++ examserver/src/main/java/com/exam/services/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.exam.services.impl;
+
+import com.exam.entity.User;
+import com.exam.entity.UserRole;
+import com.exam.services.UserService;
+
+import java.util.Set;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(User user, Set<UserRole> userRoles) {
+        return null;
+    }
+}
