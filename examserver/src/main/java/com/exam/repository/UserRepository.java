a177552868a4afa097d5a28e004880ffd5cbaca7
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 12:30:53 +0530

    Creating Repositories for entities

---
+++ examserver/src/main/java/com/exam/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.exam.repository;
+
+import com.exam.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
