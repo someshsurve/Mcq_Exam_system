9cb7100e1ec445eddab01d7f9e57b6f653582b09
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 23:01:08 +0530

    routing apps

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {AppRoutingModule} from "./app-routing.module";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
