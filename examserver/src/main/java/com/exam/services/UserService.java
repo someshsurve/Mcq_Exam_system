28b2a2adc5c352002da98486228648270db66f37
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 12:36:02 +0530

    Adding services for repositories

---
+++ examserver/src/main/java/com/exam/services/UserService.java
@@ -0,0 +1,12 @@
+package com.exam.services;
+
+import com.exam.entity.User;
+import com.exam.entity.UserRole;
+
+import java.util.Set;
+
+public interface UserService {
+
+    //creating user
+    public User createUser(User user, Set<UserRole> userRoles);
+}
