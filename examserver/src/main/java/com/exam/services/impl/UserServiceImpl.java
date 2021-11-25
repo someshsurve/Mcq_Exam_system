4c3348e0759bf8bee86d29504c56594cc6020a6d
Somesh Surve <someshsurve21@gmail.com>
2021-11-25 12:38:37 +0530

    Updating implementation

--- examserver/src/main/java/com/exam/services/impl/UserServiceImpl.java
+++ examserver/src/main/java/com/exam/services/impl/UserServiceImpl.java
@@ -2,11 +2,21 @@ package com.exam.services.impl;
 
 import com.exam.entity.User;
 import com.exam.entity.UserRole;
+import com.exam.repository.RoleRepository;
+import com.exam.repository.UserRepository;
 import com.exam.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
 
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public User createUser(User user, Set<UserRole> userRoles) {
         return null;
