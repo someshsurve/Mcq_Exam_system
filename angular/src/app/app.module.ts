61349acc516d6c085e225015b107a6d039205bb1
Somesh Surve <someshsurve21@gmail.com>
2021-11-26 16:27:32 +0530

    creating nav bar and changing things

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -17,6 +17,8 @@ import {HttpClientModule} from "@angular/common/http";
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HomeComponent } from './pages/home/home.component';
 import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -40,6 +42,8 @@ import {MatCardModule} from "@angular/material/card";
     HttpClientModule,
     MatSnackBarModule,
     MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
