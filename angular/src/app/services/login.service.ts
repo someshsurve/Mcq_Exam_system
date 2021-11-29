72b3429583ebbb92345ef5b12d8f7f2f58dbce51
Somesh Surve <someshsurve21@gmail.com>
2021-11-29 22:43:22 +0530

    setting local path to access tokens and making request

--- angular/src/app/services/login.service.ts
+++ angular/src/app/services/login.service.ts
@@ -13,4 +13,54 @@ export class LoginService {
   public generateToken(loginData: any) {
     return this.http.post(`${baseUrl}/generate-token`, loginData)
   }
+
+  //login user : set token in local storage
+  public loginUser(token: any) {
+    localStorage.setItem('token', token);
+    return true;
+  }
+
+  //islogin : user is logged in or not
+  public isLoggedIn() {
+    let tokenStr = localStorage.getItem('token');
+    if (tokenStr == undefined || tokenStr == '' || tokenStr == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //logOut : remove token from local storage
+  public logOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  //get token
+  public getToken() {
+    return localStorage.getItem('token');
+  }
+
+  //set user details
+  public setUser(user:any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  //get user
+  public getUser() {
+    let userStr = localStorage.getItem('user');
+    if (userStr != null) {
+      return JSON.parse(userStr);
+    } else {
+      this.logOut();
+      return null;
+    }
+  }
+
+  // get user role
+  public getUserRole() {
+    let user = this.getUser();
+    return user.authorities[0].authority;
+  }
 }
